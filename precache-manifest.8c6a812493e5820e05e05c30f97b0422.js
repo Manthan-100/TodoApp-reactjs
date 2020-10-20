e94061c4d584cb26eb8676a52c9e1045bf3c0a87
manthan <manthanshukla08@gmail.com>
2020-10-20 12:48:45 +0530

    Updates

---
+++ precache-manifest.8c6a812493e5820e05e05c30f97b0422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9bb753151628e84d3323d6bbd5f69a",
+    "url": "/TodoApp-reactjs/index.html"
+  },
+  {
+    "revision": "9a47b6fb419ce4a5681d",
+    "url": "/TodoApp-reactjs/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "339ce7ab35c36a927802",
+    "url": "/TodoApp-reactjs/static/css/main.dcc83da4.chunk.css"
+  },
+  {
+    "revision": "9a47b6fb419ce4a5681d",
+    "url": "/TodoApp-reactjs/static/js/2.9b401028.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp-reactjs/static/js/2.9b401028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339ce7ab35c36a927802",
+    "url": "/TodoApp-reactjs/static/js/main.48f26193.chunk.js"
+  },
+  {
+    "revision": "dca437d0f3166db8bae1",
+    "url": "/TodoApp-reactjs/static/js/runtime-main.aebd4870.js"
+  }
+]);
